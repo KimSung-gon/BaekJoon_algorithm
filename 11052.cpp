2157857c1d4ee451017ebda5e4aabb3b22468a8b
KimSung-gon <gonni27@kookmin.ac.kr>
2017-08-02 13:39:57 +0900

    Add files via upload

--- 11052.cpp
+++ 11052.cpp
@@ -3,7 +3,7 @@ int dpProcess(int num, int rest);
 using namespace std;
 
 int N[1001];
-int P[10001]; 
+int P[10001];
 int dp[1001];
 
 int main() {
@@ -11,7 +11,7 @@ int main() {
 	int num;
 	scanf("%d", &num);
 
-	for (int i = 1; i <= num; i++) 
+	for (int i = 1; i <= num; i++)
 		scanf("%d", &P[i]);
 
 	dp[1] = P[1];
@@ -22,7 +22,13 @@ int dpProcess(int num, int rest) {
 
 	if (rest == 0)
 		return 0;
-
-	for(int i=1; i<=num; i++)
-		return max(dpProcess(num, rest - i) + P[i], P[rest]);
+	if (dp[rest] != 0)
+		return dp[rest];
+	int temp;
+	for (int i = 1; i <= rest; i++) {
+		temp = max(dp[i] + dpProcess(num, rest - i), P[i]);
+		if (temp > dp[rest])
+			dp[rest] = temp;
+	}
+	return dp[rest];
 }
