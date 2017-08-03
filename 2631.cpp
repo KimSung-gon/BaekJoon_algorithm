6c3033fdb0691a3b5a34f06e0e8f03398a04ed2c
KimSung-gon <gonni27@kookmin.ac.kr>
2017-08-03 13:28:45 +0900

    Add files via upload

--- 2631.cpp
+++ 2631.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int num;
 
+int lowerBound(int start, int last, int target);
+
 int input[601];
 int temp[201];
 int dp[201];
@@ -11,16 +13,34 @@ int main() {
 	scanf("%d", &num);
 
 	int len = 0;
-	dp[1] = 1;
-	int pos = 1;
+	int pos = 0;
 
 	for (int i = 1; i <= num; i++) {
 		scanf("%d", &input[i]);
-		auto pos = lower_bound(temp + 1, temp + len + 1, input[i]);
-		*pos = input[i];
-		dp[i] = distance(temp, pos);
-		if (pos == temp + len + 1)
+		pos = lowerBound(1, len + 1, input[i]);
+		
+		dp[i] = pos;
+		temp[pos] = input[i];
+		if (pos == len + 1)
 			len++;
 	}
 	printf("%d\n", num - len);
+}
+
+int lowerBound(int start, int last, int target) {
+
+	int midTemp = start;
+	int mid = (start + last) / 2;
+
+	if (start == last)
+		return start;
+
+	if (target == temp[mid])
+		return mid;
+
+	if (target > temp[mid])
+		lowerBound(mid + 1, last, target);
+	else if (target < temp[mid])
+		lowerBound(start, mid, target);
+
 }
